a065e26c18fa253f4f5eed2c339089d8a8a8749c
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-03-09 23:55:12 -0500

    Deployment script added

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,16 @@
-# !/usr/env bash
+#!/bin/sh
 
+printf "Deploying App...\n"
+
+POE="index.js"
+PR= echo | ps ax | grep $POE | grep -v grep | awk '{print $1}' | xargs kill > /dev/null 2>&1
+
+printf "\nDeploying server...\n"
+printf "\nDownloading dependencies\n"
+
+npm install
+npm run build
+nohup node server > "$POE.out" 2> "$POE.err" &
+
+printf "\nProcess PID: "
+echo $!
